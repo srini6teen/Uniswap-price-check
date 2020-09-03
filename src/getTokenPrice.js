8b7def10c0f51700a057321c6a7c9a252c55cecd
vikssevishnu <vikassevishnu@yahoo.com>
2020-09-03 15:47:48 -0600

    new token added and exception handling done

--- src/getTokenPrice.js
+++ src/getTokenPrice.js
@@ -1,14 +1,27 @@
-  const UNISWAP = require('@uniswap/sdk')
+const UNISWAP = require("@uniswap/sdk");
 
-  const getTokenPriceWithDecimals = async (tokenAddress, decimal) => {
-  const DAI = new UNISWAP.Token(UNISWAP.ChainId.MAINNET, tokenAddress, decimal);
-  const pair = await UNISWAP.Fetcher.fetchPairData(DAI, UNISWAP.WETH[DAI.chainId]);
-  const route = new UNISWAP.Route([pair], UNISWAP.WETH[DAI.chainId]);
-
+const getTokenPriceWithDecimals = async (tokenAddress, decimal) => {
   try {
-    let tokenAmount = new UNISWAP.TokenAmount(UNISWAP.WETH[DAI.chainId], "1000000000000000000");
+    const DAI = new UNISWAP.Token(
+      UNISWAP.ChainId.MAINNET,
+      tokenAddress,
+      decimal
+    );
+    const pair = await UNISWAP.Fetcher.fetchPairData(
+      DAI,
+      UNISWAP.WETH[DAI.chainId]
+    );
+    const route = new UNISWAP.Route([pair], UNISWAP.WETH[DAI.chainId]);
+    let tokenAmount = new UNISWAP.TokenAmount(
+      UNISWAP.WETH[DAI.chainId],
+      "1000000000000000000"
+    );
 
-    const trade = new UNISWAP.Trade(route, tokenAmount, UNISWAP.TradeType.EXACT_INPUT);
+    const trade = new UNISWAP.Trade(
+      route,
+      tokenAmount,
+      UNISWAP.TradeType.EXACT_INPUT
+    );
 
     return trade.executionPrice.toSignificant(6);
   } catch (err) {
@@ -18,5 +31,5 @@
 };
 
 module.exports = {
-  getTokenPriceWithDecimals
-}
+  getTokenPriceWithDecimals,
+};
