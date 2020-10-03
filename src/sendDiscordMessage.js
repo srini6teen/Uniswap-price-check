609c1e295a628c31287efa818465020f4525ab9d
vikssevishnu <vikassevishnu@yahoo.com>
2020-10-02 19:34:13 -0600

    settings.yaml file path updated

--- src/sendDiscordMessage.js
+++ src/sendDiscordMessage.js
@@ -1,11 +1,13 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const fs = require("fs");
+const path = require("path");
 const tokenPrice = require("./getTokenPrice");
 const jsYaml = require("js-yaml");
 const e = require("express");
 
-const settings = jsYaml.safeLoad(fs.readFileSync("../settings.yaml"));
+const file = fs.readFileSync(path.resolve(__dirname, "../settings.yaml"));
+const settings = jsYaml.safeLoad(file);
 const discordSettings = settings.discord;
 client.login(discordSettings.token);
 
